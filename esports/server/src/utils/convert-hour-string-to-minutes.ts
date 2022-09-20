ff533ad413da870ad29fe563831a249684f4b2d6
itagabriel <ita32.gabriel@gmail.com>
2022-09-20 10:55:17 -0300

    projeto nlw_rocketseat react_node

---
+++ esports/server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,9 @@
+// 18:00 -> ["18", "00"] -> [18 , 00]
+
+export function convertHourStringToMinutes(hourString: string) {
+    const [hours, minutes] = hourString.split(':').map(Number)
+
+    const minutesAmout = (hours * 60) + minutes;
+
+    return minutesAmout;
+}
